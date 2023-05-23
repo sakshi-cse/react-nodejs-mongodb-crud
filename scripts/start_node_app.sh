57d832bcef336bc4cda65422dc0be3c0b3da8456
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-05-23 10:09:04 +0530

    Create start_node_app.sh

---
+++ scripts/start_node_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Go to the application directory
+cd /path/to/application
+
+# Start the Node.js application
+node app.js  # Replace "app.js" with the main file of your application
+
+# Exit the script with a success status
+exit 0
