1da690d7cb478dc515efbb92d429401bdf2bba22
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-05-23 10:06:50 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Go to the application directory
+cd /path/to/application
+
+# Install Node.js dependencies using npm
+npm install
+
+# Additional commands if required
+# For example, installing global dependencies or running additional setup tasks
+# npm install -g some-package
+# ./setup.sh
+
+# Exit the script with a success status
+exit 0
